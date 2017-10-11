b43949757a23b91e7fb0cec8cbd79c524b556399
KirukatoD <vanya1817@yandex.ru>
2017-10-11 22:09:19 +0300

    Добавьте файлы проекта.

---
+++ TestTask/FixedThreadPool.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    public class FixedThreadPool
+    {
+        
+
+        enum Prioryty
+        {
+            HIGH,
+            NORMAL,
+            LOW
+        }
+
+        public FixedThreadPool(int threads)
+        {
+
+        }
+
+        bool Execute(Task task, Prioryty priority)
+        {
+
+            return false;
+        }
+
+        void  Stop()
+        {
+
+        }
+    }
+}
---
+++ TestTask/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    public class Task
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Something to do...");
+        }
+    }
+}
